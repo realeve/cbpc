c4a6ae5b3e9edef31ac5f92211bf4be1b73775de
realeve <realeve@qq.com>
2017-06-19 04:19:32 +0800

    使用FLEX布局主页，减少DOM recalc

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
+  <div class="app">
     <v-header></v-header>
-    <router-view :style="{'min-height':minHeight}"></router-view>
+    <router-view class="v-content"></router-view>
     <v-footer></v-footer>
   </div>
 </template>
 <script>
+  import './assets/css/reset.css';
   import './assets/css/navbar.css';
   import VHeader from './components/VHeader';
   import VFooter from './components/VFooter';
@@ -14,13 +15,6 @@
     components: {
       VHeader,
       VFooter
-    },
-    computed: {
-      minHeight() {
-        let topHeight = 62;
-        let bottomHeight = 124;
-        return (window.outerHeight - topHeight - bottomHeight - 4) + 'px';
-      }
     },
     data() {
       return {
@@ -41,53 +35,12 @@
 </script>
 
 <style>
-  .wrap {
-    padding-top: 62px;
-    overflow: hidden;
+  .app {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
   }
-
-  .main {
-    position: relative;
-    padding-top: 20px;
+  .v-content{
+    flex: 1;
   }
-
-  .container {
-    padding-right: 15px;
-    padding-left: 15px;
-    margin-right: auto;
-    margin-left: auto;
-  }
-
-  @media (min-width: 768px) {
-    .container {
-      width: 750px;
-    }
-  }
-
-  @media (min-width: 992px) {
-    .container {
-      width: 970px;
-    }
-  }
-
-  @media (min-width: 1200px) {
-    .container {
-      width: 1170px;
-    }
-  }
-
-.clearfix:before,
-.clearfix:after{
-  display: table;
-  content: " ";
-}
-.clearfix:after {
-  clear: both;
-}
-.pull-right {
-  float: right !important;
-}
-.pull-left {
-  float: left !important;
-}
 </style>
